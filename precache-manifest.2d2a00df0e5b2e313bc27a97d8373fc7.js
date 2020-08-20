f0b40845caf5b88fb60220fa8ddd5788c9b6a94f
taajuddin <taajraza@gmail.com>
2020-08-20 21:54:40 +0530

    Updates

---
+++ precache-manifest.2d2a00df0e5b2e313bc27a97d8373fc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f04560d6dde8cc86c21fcb626597bec2",
+    "url": "/todo-redux-app/index.html"
+  },
+  {
+    "revision": "2757167637961e3c16d6",
+    "url": "/todo-redux-app/static/css/main.b6d31af6.chunk.css"
+  },
+  {
+    "revision": "6df27d7add2976cc7899",
+    "url": "/todo-redux-app/static/js/2.938cd8ba.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/todo-redux-app/static/js/2.938cd8ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2757167637961e3c16d6",
+    "url": "/todo-redux-app/static/js/main.d5f137cf.chunk.js"
+  },
+  {
+    "revision": "79ede07bcbc25b62dca9",
+    "url": "/todo-redux-app/static/js/runtime-main.284e90d5.js"
+  }
+]);
